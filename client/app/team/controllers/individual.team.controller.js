6ab56c5f8a9e7ffb6496af2d246d4885c8bb6fa3
davidyu37 <davidyu37@gmail.com>
2015-12-23 22:48:08 +0800

    team edit districts and cities

--- client/app/team/controllers/individual.team.controller.js
+++ client/app/team/controllers/individual.team.controller.js
@@ -110,6 +110,80 @@ angular.module('keepballin')
       $scope.edit = !($scope.edit);
     };
 
+
+    //District selections
+    $scope.districts = [
+      '北部',
+      '中部',
+      '南部',
+      '東部',
+      '外島'
+    ];
+
+    //Display cities within the districts
+    $scope.getCities = function(district) {
+        switch(district) {
+            case '北部':
+                $scope.cities = [
+                    '台北市',
+                    '新北市',
+                    '基隆市',
+                    '桃園縣',
+                    '新竹縣',
+                    '新竹市',
+                    '宜蘭縣'
+                ];
+                break;
+            case '中部':
+                $scope.cities = [
+                    '台中市',
+                    '台中縣',
+                    '苗栗縣',
+                    '彰化縣',
+                    '雲林縣',
+                    '南投縣'
+                ];
+                break;
+            case '南部':
+                $scope.cities = [
+                    '嘉義市',
+                    '嘉義縣',
+                    '台南市',
+                    '台南縣',
+                    '高雄市',
+                    '高雄縣',
+                    '屏東縣'
+                ];
+                break;
+            case '東部':
+                $scope.cities = [
+                    '花蓮市',
+                    '花蓮縣',
+                    '台東縣',
+                    '台東市'
+                ];
+                break;
+            case '外島':
+                $scope.cities = [
+                    '金門',
+                    '小金門',
+                    '澎湖',
+                    '馬祖',
+                    '蘭嶼',
+                    '琉球',
+                    '綠島',
+                    '外傘頂洲',
+                    '太平島'
+                ];
+                break; 
+            default:
+                $scope.cities = [];
+                return;
+
+        }
+
+    };
+
     $scope.changeToMe = function(me, type) {
         switch (type) {
           case 'contact': 
