bdcd6c6d8c10ce0f0929904962f06d5c9e5833b2
davidyu37 <davidyu37@gmail.com>
2015-12-25 21:35:47 +0800

    chrome option style fix attempt 2

--- client/app/teammate/teammate.controller.js
+++ client/app/teammate/teammate.controller.js
@@ -1,6 +1,81 @@
 'use strict';
 
 angular.module('keepballin')
-  .controller('TeammateCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('TeammateCtrl', ['$scope', function ($scope) {
+    $scope.districts = [
+      '北部',
+      '中部',
+      '南部',
+      '東部',
+      '外島',
+      '顯示全部'
+    ];
+
+    $scope.getCities = function(district) {
+        switch(district) {
+            case '北部':
+                $scope.cities = [
+                    '台北市',
+                    '新北市',
+                    '基隆市',
+                    '桃園縣',
+                    '新竹縣',
+                    '新竹市',
+                    '宜蘭縣'
+                ];
+                break;
+            case '中部':
+                $scope.cities = [
+                    '台中市',
+                    '台中縣',
+                    '苗栗縣',
+                    '彰化縣',
+                    '雲林縣',
+                    '南投縣'
+                ];
+                break;
+            case '南部':
+                $scope.cities = [
+                    '嘉義市',
+                    '嘉義縣',
+                    '台南市',
+                    '台南縣',
+                    '高雄市',
+                    '高雄縣',
+                    '屏東縣'
+                ];
+                break;
+            case '東部':
+                $scope.cities = [
+                    '花蓮市',
+                    '花蓮縣',
+                    '台東縣',
+                    '台東市'
+                ];
+                break;
+            case '外島':
+                $scope.cities = [
+                    '金門',
+                    '小金門',
+                    '澎湖',
+                    '馬祖',
+                    '蘭嶼',
+                    '琉球',
+                    '綠島',
+                    '外傘頂洲',
+                    '太平島'
+                ];
+                break;
+            case '顯示全部':
+                $scope.cities = [];
+                $scope.search = {};
+                break;
+
+            default:
+                $scope.cities = [];
+                return;
+
+        }
+
+    };
+  }]);
